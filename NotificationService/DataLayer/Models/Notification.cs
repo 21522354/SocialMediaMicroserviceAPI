17c0648244e09f9432c9b2b750cae1a1d0b1c583
Nam Đàm Thành <21522354@ms.uit.edu.vn>
2024-11-14 14:26:01 +0700

    install package rabbitmq client

---
+++ NotificationService/DataLayer/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace NotificationService.DataLayer.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Message { get; set; }
+        public Guid PostId { get; set; }        
+        public bool IsAlreadySeen { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
