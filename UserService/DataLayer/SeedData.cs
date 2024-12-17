eeb7a4e06ebdb6c0fd2ba01208a731016c951891
21522354 <120326395+21522354@users.noreply.github.com>
2024-12-17 08:38:20 +0700

    fix bug seed data userservice

--- UserService/DataLayer/SeedData.cs
+++ UserService/DataLayer/SeedData.cs
@@ -14,95 +14,99 @@ namespace UserService.DataLayer
 
                 _context.Database.Migrate();
 
-                Console.WriteLine("Seeding data");
-                _context.Users.AddRange(
-                    new User()
-                    {
-                        UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
-                        NickName = "nva_123",
-                        FullName = "Nguyen Van A",
-                        Email = "ndam8175@gmail.com",
-                        Password = "123123",
-                        Avatar = "abc.png",
-                        FbId = "asdadfasdf"
-                    },
-                    new User()
-                    {
-                        UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
-                        NickName = "nvb_123",
-                        FullName = "Nguyen Van B",
-                        Email = "ndam8175@gmail.com",
-                        Password = "123123",
-                        Avatar = "abc.png",
-                        FbId = "asdadfasdf"
-                    },
-                    new User()
-                    {
-                        UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3"),
-                        FullName = "Nguyen Van C",
-                        NickName = "nvc_123",
-                        Email = "ndam8175@gmail.com",
-                        Password = "123123",
-                        Avatar = "abc.png",
-                        FbId = "asdadfasdf"
-                    },
-                    new User()
-                    {
-                        UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4"),
-                        NickName = "nvd_123",
-                        FullName = "Nguyen Van D",
-                        Email = "ndam8175@gmail.com",
-                        Password = "123123",
-                        Avatar = "abc.png",
-                        FbId = "asdadfasdf"
-                    },
-                    new User()
-                    {
-                        UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d5"),
-                        NickName = "nve_123",
-                        FullName = "Nguyen Van E",
-                        Email = "ndam8175@gmail.com",
-                        Password = "123123",
-                        Avatar = "abc.png",
-                        FbId = "asdadfasdf"
-                    });
-                _context.Follows.AddRange(
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3")
-                    },
-                    new UserFollow()
-                    {
-                        UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3"),
-                        UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
-                    });
-                _context.SaveChanges();     
+                if(!_context.Users.Any())
+                {
+                    Console.WriteLine("Seeding data");
+                    _context.Users.AddRange(
+                        new User()
+                        {
+                            UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
+                            NickName = "nva_123",
+                            FullName = "Nguyen Van A",
+                            Email = "ndam8175@gmail.com",
+                            Password = "123123",
+                            Avatar = "abc.png",
+                            FbId = "asdadfasdf"
+                        },
+                        new User()
+                        {
+                            UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
+                            NickName = "nvb_123",
+                            FullName = "Nguyen Van B",
+                            Email = "ndam8175@gmail.com",
+                            Password = "123123",
+                            Avatar = "abc.png",
+                            FbId = "asdadfasdf"
+                        },
+                        new User()
+                        {
+                            UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3"),
+                            FullName = "Nguyen Van C",
+                            NickName = "nvc_123",
+                            Email = "ndam8175@gmail.com",
+                            Password = "123123",
+                            Avatar = "abc.png",
+                            FbId = "asdadfasdf"
+                        },
+                        new User()
+                        {
+                            UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4"),
+                            NickName = "nvd_123",
+                            FullName = "Nguyen Van D",
+                            Email = "ndam8175@gmail.com",
+                            Password = "123123",
+                            Avatar = "abc.png",
+                            FbId = "asdadfasdf"
+                        },
+                        new User()
+                        {
+                            UserId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d5"),
+                            NickName = "nve_123",
+                            FullName = "Nguyen Van E",
+                            Email = "ndam8175@gmail.com",
+                            Password = "123123",
+                            Avatar = "abc.png",
+                            FbId = "asdadfasdf"
+                        });
+                    _context.Follows.AddRange(
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d1")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d2"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3")
+                        },
+                        new UserFollow()
+                        {
+                            UserFromId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d3"),
+                            UserToId = Guid.Parse("e0be4a36-67cd-4dd6-be48-8b800c3123d4")
+                        });
+                    _context.SaveChanges();
+                }
+                 
             }
         }
     }
